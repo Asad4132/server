e8af85255d95248d1756f766e5adcbd74da4f294
asad4132 <l154132@lhr.nu.edu.pk>
2024-01-28 00:44:03 +0500

    add google auth

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "Hello World" });
-});
+app.use(
+  cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = process.env.PORT || 3001
+require("./routes/authRoutes")(app);
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 
 // http://localhost:3001
+// http://localhost:3001/auth/google
